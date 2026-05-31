2dcf3804f217f203bdd92584157c411b4e637dbe
jsw-teams <jsw-teams@users.noreply.github.com>
2026-05-31 17:22:11 +0800

    Update robots policy

--- frontend/src/pages/robots.txt.ts
+++ frontend/src/pages/robots.txt.ts
@@ -1,6 +1,15 @@
 export function GET() {
   return new Response(
-    `User-agent: *
+    `# Claude is not welcome here because this site owner does not welcome
+# unethical AI crawlers that freely scrape sites while arbitrarily
+# banning user accounts.
+User-agent: ClaudeBot
+Disallow: /
+
+User-agent: Claude-User
+Disallow: /
+
+User-agent: *
 Allow: /
 
 Sitemap: https://dns.js.gripe/sitemap.xml
